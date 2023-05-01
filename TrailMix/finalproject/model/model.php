7cf2620afb830d292f5b2d9d6b1520142d556711
c-hankey <81665821+c-hankey@users.noreply.github.com>
2023-05-01 10:06:17 -0400

    Edit and delete binary data

--- TrailMix/finalproject/model/model.php
+++ TrailMix/finalproject/model/model.php
@@ -88,6 +88,7 @@
             $statement->execute();
             $result = $statement->fetch(); // Should be 0 or 1 row
             $statement->closeCursor();
+            $result['TrailImagePath'] = checkTrailImagePath($trailID);
             return $result; // False if 0 rows
         } catch (PDOException $e) {
             $errorMessage = $e->getMessage();
@@ -112,11 +113,43 @@
         return $memberArray;
     }
 
-    function insertTrail($name, $description, $location, $distance, $difficulty, $loop, $bike, $activehours, $activeseason){
-        //Insert code to add a trail to database here
+    function getTrailImagePath($trailID) {
+        $trailImageDirectory = "../DataFiles/TrailImages";
+        return $trailFilePath = "$trailImageDirectory/$trailID.jpg";
     }
 
-    function updateTrail($trailID, $name, $description, $location, $distance, $difficulty, $loop, $bike, $activehours, $activeseason) {
+    function checkTrailImagePath($trailID) {
+        $trailFilePath = getTrailImagePath($trailID);
+        if(is_file($trailFilePath)) {
+            return $trailFilePath;
+        } else {
+            return "";
+        }
+    }
+
+    function saveTrailImageFile ($trailID, $tempImageFilePath) {
+        if($tempImageFilePath != "") {
+            $newImagePath = getTrailImagePath($trailID);
+            if(move_uploaded_file($tempImageFilePath, $newImagePath) == FALSE) {
+                $errorMessage = "Unable to move the image file.";
+                include '../view/errorPage.php';
+            }
+        }
+    }
+
+    function insertTrail($name, $description, $location, $distance, $difficulty, $loop, $bike, $activehours, $activeseason, $tempImageFilePath){
+        //Insert code to add a trail to database here
+
+        //Uncomment this code for the success-- it was touched on in a later lecture
+        /*if($succes) {
+            saveTrailImageFile($db->lastInsetId(), $tempImageFilePath);
+            return $db->lastInsertId();
+        } else {
+            logSQLError($statement->errorInfo());
+        }*/
+    }
+
+    function updateTrail($trailID, $name, $description, $location, $distance, $difficulty, $loop, $bike, $activehours, $activeseason, $tempImageFilePath, $deleteImage) {
         $db = getDBConnection();
         $query = 'UPDATE trail SET Name = :Name, Description = :Description, Location = :Location, Distance = :Distance,
                   Difficulty = :Difficulty, Loops = :Loop, Bike = :Bike, ActiveHours = :ActiveHours, ActiveSeason = :ActiveSeason
@@ -136,6 +169,10 @@
         $success = $statement->execute();
         $statement->closeCursor();
         if($success) {
+            saveTrailImageFile($trailID, $tempImageFilePath);
+            if($deleteImage && $tempImageFilePath == "") {
+                deleteTrailImageFile($trailID);
+            }
             return $statement->rowCount();
         } else {
             logSQLError($statement->errorInfo());
@@ -151,11 +188,23 @@
         $success = $statement->execute();
         $statement->closeCursor();
         if ($success) {
+            deleteTrailImageFile($trailID);
             return $statement->rowCount(); // Number of rows affected
         } else {
             logSQLError($statement->errorInfo()); // Log error
         }
     }
+
+    function deleteTrailImageFile($trailID) {
+        $imageFilePath = checkTrailImagePath($trailID);
+        if($imageFilePath != "") {
+            if(unlink($imageFilePath) == FALSE) {
+                $errorMessage = "Unable to delete image file at $imageFilePath";
+                include '../view/errorPage.php';
+            }
+        }
+    }
+
 
     function logSQLError($error) {
         $errorMessage = $error[2];
